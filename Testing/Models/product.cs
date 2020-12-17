e09ff77af4fa3bbeb166d758ca6db31093af7235
promobius <matt.huebert@gmail.com>
2020-12-17 11:22:10 -0600

    completed

--- Testing/Models/product.cs
+++ Testing/Models/product.cs
@@ -5,7 +5,16 @@ using System.Threading.Tasks;
 
 namespace Testing.Models
 {
-	public class product
+	public class Product
 	{
+		public int ProductID { get; set;}
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public int CategoryID { get; set; }
+		public int OnSale { get; set; }
+		public int StockLevel { get; set; }
+
+		public string Image { get; set; }
+		public IEnumerable<Category> Categories { get; set; }
 	}
 }
